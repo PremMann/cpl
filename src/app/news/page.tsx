b59454282a78216325efda0e2f30c4cd8ff30eff
Prem MANN <premmann@GG-2.local>
2025-02-01 23:01:43 +0700

    dg

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return <>
     <div className="text-center pt-32 px-5">
-<h1 className="text-4xl md:text-5xl font-bold mb-5">News</h1>
+<h1 className="text-4xl md:text-5xl font-bold mb-5">News GG</h1>
 <p className="max-w-[750px] mx-auto leading-8 text-center">
   Blog is simply dummy text of the printing and typesetting industry.
 </p>

b65775b392838da8220ddcf1526643b6dd77c45e
Prem MANN <premmann@GG-2.local>
2025-02-10 06:37:59 +0700

    update get static prop

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,35 +1,33 @@
-import { getAllUsers, getAllPosts } from "@/actions/createUser";
+import prisma from '@/lib/db';
+
+async function fetchUsers() {
+  const users = await prisma.user.findMany();
+  return users;
+}
+
+async function fetchPosts() {
+  const posts = await prisma.post.findMany();
+  return posts;
+}
 
 export default async function UserPage() {
- const users = await getAllUsers();
- const posts = await getAllPosts();
-    console.log("posts : ", posts);
- console.log("users : ", users);
+  const users = await fetchUsers();
+  const posts = await fetchPosts();
 
   return (
     <div>
       <h1>Users</h1>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
-            <span>{user.id}</span>
-            <span>{user.username}</span>
-            <span>{user.email}</span>
-            <span>{user.role}</span>
-            <span>{user.name}</span>
-          </li>
+          <li key={user.id}>{user.username} - {user.email}</li>
         ))}
       </ul>
       <h1>Posts</h1>
-        <ul>
-            {posts.map((post) => (
-            <li key={post.id}>
-                <span>{post.id}</span>
-                <span>{post.title}</span>
-                <span>{post.content}</span>
-            </li>
-            ))}
-        </ul>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title} - {post.content}</li>
+        ))}
+      </ul>
     </div>
   );
 }

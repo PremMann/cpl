334a3d64d16effedbde265b2e46e1f1c2369d4c6
Prem MANN <premmann@GG-2.local>
2025-08-17 17:04:38 +0700

    update before pushing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import HomeLanding from "@/components/homeLanding";
-import LoanCalculator from "@/components/loancalculation";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-red-50 to-white py-1 pb-5">
       <HomeLanding />
-      {/* <div className="mt-14 px-4">
-        <LoanCalculator />
-      </div> */}
     </div>
   );
 }

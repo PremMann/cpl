87a86ec0bf3daad65494ab40e414ff2a58ae42af
Prem MANN <premmann@GG-2.local>
2025-08-18 23:06:49 +0700

    remove to compilt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
-import Header01 from "@/components/header01";
+// import Header01 from "@/components/header01";
 import Footer from "@/components/footer";
 import Container from "@/components/container";
 import { NextUIProvider } from "@nextui-org/system";

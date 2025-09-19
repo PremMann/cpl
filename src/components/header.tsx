2416cf75690a98d4d7738ee8d5ac5c52d0af4d32
Prem MANN <prem.mann@apmfi.com.kh>
2025-09-19 17:16:39 +0700

    updae careers url

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from './ui/dropdown-menu';
+import { late } from 'zod';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -37,12 +38,13 @@ export default function Header() {
       label: t('products').toUpperCase(),
       href: '/products',
       children: [
+        { latebel: t('all_products'), href: '/products' },
         { label: 'Quick Loan', href: '/products/quick-loan' },
         { label: 'Electronics Installment Loan', href: '/products/electronics-installment-loan' },
         { label: 'Motorbike Installment Loan', href: '/products/motorbike-installment-loan' },
-        // { label: 'Car Installment Loan', href: '/products/car-installment-loan' },
-        // { label: 'Land Installment Loan', href: '/products/land-installment-loan' },
-        // { label: 'Secured Installment Loan', href: '/products/secured-installment-loan' }
+        { label: 'Car Installment Loan', href: '/products/car-installment-loan' },
+        { label: 'Land Installment Loan', href: '/products/land-installment-loan' },
+        { label: 'Secured Installment Loan', href: '/products/secured-installment-loan' }
       ]
     },
     { label: t('promotion').toUpperCase(), href: '/promotions' },
@@ -60,7 +62,7 @@ export default function Header() {
   <Link href="/calculation" className="hover:underline">{t('loan_calculator')}</Link>
   <Link href="/" className="hover:underline">{t('loan_car')}</Link>
   <Link href="/" className="hover:underline">{t('apply_loan')}</Link>
-  <Link href="/" className="hover:underline">{t('career')}</Link>
+  <Link href="/careers" className="hover:underline">{t('career')}</Link>
   <button onClick={() => i18n.changeLanguage('en')} className="hover:underline">EN</button>
   <button onClick={() => i18n.changeLanguage('kh')} className="hover:underline">KH</button>
       </div>
